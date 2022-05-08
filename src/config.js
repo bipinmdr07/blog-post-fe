9beeb754d31a5a82be8bca29cb987c27129509b5
Bipin Manandhar <kubipin07@gmail.com>
2022-05-08 07:08:37 +0545

    Add necessary environment variables for github oauth.

--- src/config.js
+++ src/config.js
@@ -1,6 +1,13 @@
 const config = {
   env: process.env.NODE_ENV || 'development',
   basename: process.env.REACT_APP_BASE_NAME || '/',
+  oauth: {
+    github: {
+      redirectURL: process.env.REACT_APP_GITHUB_OAUTH_URL,
+      clientId: process.env.REACT_APP_GITHUB_CLIENT_ID,
+      clientSecret: process.env.REACT_APP_CLIENT_SECRET,
+    },
+  },
 };
 
 export default config;
