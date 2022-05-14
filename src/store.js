1aa419101557d515c2701a549153f71e19db1bfa
Bipin Manandhar <kubipin07@gmail.com>
2022-05-14 12:16:04 +0545

    Fix the warning in browser console from redux-toolkit.

--- src/store.js
+++ src/store.js
@@ -2,9 +2,11 @@ import { persistStore } from 'redux-persist';
 import { configureStore } from '@reduxjs/toolkit';
 
 import rootReducer from './reducers';
+import thunk from 'redux-thunk';
 
 const store = configureStore({
   reducer: rootReducer,
+  middleware: [thunk],
 });
 
 const persistor = persistStore(store);
