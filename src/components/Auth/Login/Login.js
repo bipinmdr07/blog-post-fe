ea94f6dca157985b288fcf6f7a7aeac7315e7569
Bipin Manandhar <kubipin07@gmail.com>
2022-05-14 18:39:08 +0545

    Halt the login component for 1 sec for showing the logging in message.

--- src/components/Auth/Login/Login.js
+++ src/components/Auth/Login/Login.js
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
+import { sleep } from 'utils/sleep';
 import { isEmpty } from 'utils/isEmpty';
 import { getUserFromToken } from 'utils/jwt';
 
@@ -9,12 +10,17 @@ import { setLoggedInUser } from 'actions/data/auth';
 
 const Login = () => {
   // eslint-disable-next-line
-  const [searchParams, setSearchParams] = useSearchParams('');
+  const [searchParams, _] = useSearchParams('');
   const dispatch = useDispatch();
   const user = useSelector(state => state.data.auth.user);
 
   useEffect(() => {
+    handleUserLogin();
+  }, [user]);
+
+  const handleUserLogin = async () => {
     if (isEmpty(searchParams.get('access_token')) || !isEmpty(user)) {
+      await sleep(1000);
       window.location = '/';
       return;
     }
@@ -22,7 +28,7 @@ const Login = () => {
     const userInfo = getUserFromToken(searchParams.get('access_token'));
 
     dispatch(setLoggedInUser(userInfo));
-  }, [user]);
+  };
 
   return (
     <>
